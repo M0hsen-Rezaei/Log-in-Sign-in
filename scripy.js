defcb15df347aebb5d70e958e810e0b03b107df1
Mohsen <mohsenrezaii315@gmail.com>
2024-04-07 15:24:26 +0330

    save User

--- scripy.js
+++ scripy.js
@@ -1,3 +1,14 @@
+//array users
+const users = [
+    {
+        username : "Ali",
+        password : "1234"
+    },
+    {
+        username : "hasan",
+        password : "1111"
+    },
+]
 //Defined containers & buttons
 const formLogin = document.getElementById("form-login")
 const formSignin = document.getElementById("form-signin")
@@ -35,9 +46,11 @@ btnSignin.addEventListener("click" , activateSignin)
 btnSubmitLogin.addEventListener("click" , (event)=>{
     event.preventDefault()
     checkInputs()
+    console.log(users);
 })
 btnSubmitSignin.addEventListener("click" , (event)=>{
     event.preventDefault()
+    checkInputs()
 })
 
 //error handling
@@ -47,6 +60,7 @@ const errorInput = function(input , message){
 }
 
 //check input
+let saveUser = true
 const checkInputs = function(){
     const emailLoginValue = loginEmail.value.trim()
     const usernameLoginValue = loginUsername.value.trim()
@@ -54,11 +68,13 @@ const checkInputs = function(){
     const password2LoginValue = loginPassword2.value.trim()
     const usernameSigninValue = signinUsername.value.trim()
     const passwordSigninValue = signinPassword.value.trim()
+    //check log in inputs
 
     if(emailLoginValue == ""){
         errorInput(loginEmail , "pleas insert email")
         loginEmail.classList.remove("success-input")
         loginEmail.classList.add("eror-input")
+        saveUser = false
     }else{
         errorInput(loginEmail , "")
         loginEmail.classList.remove("eror-input")
@@ -68,6 +84,7 @@ const checkInputs = function(){
         errorInput(loginUsername , "pleas insert username")
         loginUsername.classList.remove("success-input")
         loginUsername.classList.add("eror-input")
+        saveUser = false
     }else{
         errorInput(loginUsername , "")
         loginUsername.classList.remove("eror-input")
@@ -77,6 +94,7 @@ const checkInputs = function(){
         errorInput(loginPassword , "pleas insert password")
         loginPassword.classList.remove("success-input")
         loginPassword.classList.add("eror-input")
+        saveUser = false
     }else{
         errorInput(loginPassword , "")
         loginPassword.classList.remove("eror-input")
@@ -86,15 +104,65 @@ const checkInputs = function(){
         errorInput(loginPassword2 , "pleas insert confirm password")
         loginPassword2.classList.remove("success-input")
         loginPassword2.classList.add("eror-input")
+        saveUser = false
     }else if(password2LoginValue != passwordLoginValue){
         errorInput(loginPassword2 , "Please enter the password correctly")
         loginPassword2.classList.remove("success-input")
         loginPassword2.classList.add("eror-input")
+        saveUser = false
     }
     else{
         errorInput(loginPassword2 , "")
         loginPassword2.classList.remove("eror-input")
         loginPassword2.classList.add("success-input")
     }
-}
+    //saved user
+    if(saveUser){
+        users.push({
+            username: usernameLoginValue,
+            password: passwordLoginValue
+        })
+    }else{
+        saveUser = true
+    }
 
+    //check log in inputs
+    var userStatus = false;
+    var passwordStatus = false;
+    users.forEach(user => {
+        if(user.username == usernameSigninValue){
+            userStatus = true
+            if(user.password == passwordSigninValue){
+                passwordStatus = true
+            }
+        }
+    });
+    if(usernameSigninValue == ""){
+        errorInput(signinUsername , "pleas insert username")
+        signinUsername.classList.remove("success-input")
+        signinUsername.classList.add("eror-input")
+    }else if(!userStatus){
+        errorInput(signinUsername , "Username Not Avalibale")
+        signinUsername.classList.remove("success-input")
+        signinUsername.classList.add("eror-input")
+    }
+    else{
+        errorInput(signinUsername , "")
+        signinUsername.classList.remove("eror-input")
+        signinUsername.classList.add("success-input")
+    }
+    if(passwordSigninValue == ""){
+        errorInput(signinPassword , "pleas insert password")
+        signinPassword.classList.remove("success-input")
+        signinPassword.classList.add("eror-input")
+    }else if(!passwordStatus){
+        errorInput(signinPassword , "The password is wrong")
+        signinPassword.classList.remove("success-input")
+        signinPassword.classList.add("eror-input")
+    }
+    else{
+        errorInput(signinPassword , "")
+        signinPassword.classList.remove("eror-input")
+        signinPassword.classList.add("success-input")
+    }
+}
